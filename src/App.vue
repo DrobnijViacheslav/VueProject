851070b4547284d3bb5c59a3ebf64ae176e6308c
DrobnijViacheslav <estermanuz3223@gmail.com>
2024-04-29 14:46:28 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+import Vheader from "@/components/VHeader.vue";
+import VFooter from "@/components/VFooter.vue";
+import VPromo from "@/components/VPromo.vue";
+import VProjects from "@/components/VProjects.vue";
+import VTech from "@/components/VTech.vue";
+import VFeedback from "@/components/VFeedback.vue";
+</script>
+
+<template>
+  <vheader />
+  <main>
+    <v-promo />
+    <v-projects />
+    <v-tech />
+    <v-feedback />
+  </main>
+  <v-footer></v-footer>
+</template>
+
+<style scoped>
+
+</style>
